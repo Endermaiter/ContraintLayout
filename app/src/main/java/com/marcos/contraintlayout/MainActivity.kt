da53a9b1c93ab7df8928e9cd8afb49457b59e927
Endermaiter <mpereznovoa@danielcastelao.org>
2022-09-29 12:43:03 +0200

    Constraint Layout Implementado

---
+++ app/src/main/java/com/marcos/contraintlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.marcos.contraintlayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
